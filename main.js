add6b5bb252f21bb5ec1f778df5b212a7aa82d01
elena-1234 <59923063+elena-1234@users.noreply.github.com>
2020-05-14 15:47:16 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+<form>
+  <label for="vname">Vorname:
+    <input id="vname" name="vname">
+  </label>
+  <label for="zname">Zuname: 
+    <input id="zname" name="zname">
+  </label>
+
+  <label for="männl">männlich</label>
+    <input type="radio" id="männl" name="geschlecht" value="0">
+  <label for="weibl">weiblich</label>
+    <input type="radio" id="weibl" name="geschlecht" value="1">
+  <label for="queer">queer</label>
+    <input type="radio" id="queer" name="geschlecht" value="2">
+
+  <label for="alter">über 18:</label>
+  <input type="checkbox" id="alter" name="alter">
+
+  <input type="submit" value="senden">
+</form>
